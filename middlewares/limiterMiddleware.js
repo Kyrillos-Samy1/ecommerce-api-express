4a19dd0b91ae197915d996f812b9fd600b84f689
Kyrillos-Samy1 <kyrillossamy@outlook.com>
2025-11-11 04:00:03 +0200

    Remove rate limiting middleware
    Deletes `limiterMiddleware.js`, disabling express-rate-limit functionality.

--- middlewares/limiterMiddleware.js
+++
@@ -1,8 +0,0 @@
-const { default: rateLimit } = require("express-rate-limit");
-
-//! Rate Limiting middleware
-exports.limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!"
-});
