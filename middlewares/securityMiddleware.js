3983432b781902b699176f034dc3b9b67c0cfadc
Kyrillos-Samy1 <kyrillossamy@outlook.com>
2025-11-11 04:02:03 +0200

    Add security middleware
    - Implement rate limiting to prevent abuse and brute-force attacks
    
    - Add Helmet with Content Security Policy (CSP) to block XSS and clickjacking

---
+++ middlewares/securityMiddleware.js
@@ -0,0 +1,22 @@
+const { default: rateLimit } = require("express-rate-limit");
+const { default: helmet } = require("helmet");
+
+//! Rate Limiting middleware
+exports.limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!"
+});
+
+//! Prevent XSS attacks and clickjacking vulnerabilities by setting the X-XSS-Protection header
+exports.xssProtection = helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    imgSrc: ["'self'", "data:", "https:"],
+    scriptSrc: ["'self'", "https://cdn.jsdelivr.net"],
+    styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+    fontSrc: ["'self'", "https://fonts.gstatic.com"]
+  }
+});
+
+
