d0d1f966ad52ceb1e5d7b32b9411f20968e8a711
Kyrillos-Samy1 <kyrillossamy@outlook.com>
2025-11-11 21:04:00 +0200

    Updates security middlewares to improve protection against XSS attacks and clickjacking vulnerabilities by utilizing a Content Security Policy.

--- middlewares/mountSecurityMiddlewares.js
+++ middlewares/mountSecurityMiddlewares.js
@@ -3,18 +3,20 @@ const hpp = require("hpp");
 const ExpressMongoSanitize = require("express-mongo-sanitize");
 const { xss } = require("express-xss-sanitizer");
 
-const { limiter, xssProtection } = require("./securityMiddleware");
+const { limiter, contentSecurityPolicy } = require("./securityMiddleware");
 
 exports.mountSecurityMiddlewares = (app) => {
   //! Prevent NoSQL injection attacks
   app.use(ExpressMongoSanitize());
+
+  //! Prevent XSS attacks by using the xss middleware
   app.use(xss())
 
   //! Set security HTTP headers
   app.use(helmet());
 
   //! Prevent XSS attacks and clickjacking vulnerabilities by setting the X-XSS-Protection header
-  app.use(xssProtection());
+  app.use(contentSecurityPolicy());
 
   //! Prevent HTTP parameter pollution by using the hpp middleware
   app.use(
