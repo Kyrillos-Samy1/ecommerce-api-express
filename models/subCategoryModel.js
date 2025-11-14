e48b8c89921aa62f54f918276a5854dde9471946
Kyrillos-Samy1 <kyrillossamy@outlook.com>
2025-11-14 08:07:52 +0200

    ### Prevent subcategory deletion if linked products exist
    - Add pre-delete middleware to SubCategory schema
    
    - Stop deletion when products are associated
    
    - Throw APIError on deletion attempt with linked products
    
    - Ensure data integrity

--- models/subCategoryModel.js
+++ models/subCategoryModel.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+const APIError = require("../utils/apiError");
+const ProductModel = require("./productModel");
 
+//! 1- Create Category Schema
 const subCategorySchema = new mongoose.Schema(
   {
     name: {
@@ -22,6 +25,27 @@ const subCategorySchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+//! Prevent SubCategory Deletion if there are any products linked to it
+subCategorySchema.pre("findOneAndDelete", async function (next) {
+  const filter = this.getFilter();
+  const subCategoryId = filter._id;
+  if (!subCategoryId) return next();
+
+  const product = await ProductModel.findOne({ subCategory: subCategoryId });
+  if (product) {
+    return next(
+      new APIError(
+        "Cannot Delete SubCategory! There are products linked to this subCategory. Remove or reassign them first.",
+        400,
+        "SubCategory Deletion Error"
+      )
+    );
+  }
+
+  next();
+});
+
+//! 2- Create Model
 const SubCategoryModel = mongoose.model("SubCategory", subCategorySchema);
 
 module.exports = SubCategoryModel;
