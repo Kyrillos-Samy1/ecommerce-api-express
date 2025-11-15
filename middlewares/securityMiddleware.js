0bdbe4878fbdb620d2311627dba172350cff5714
Kyrillos-Samy1 <kyrillossamy@outlook.com>
2025-11-15 21:14:44 +0200

    ### feat(rate-limit): implement user-specific rate limiting
    - Updated the rate limiter to generate unique identifiers based on the authenticated user's ID or the request email address.
    
    - This enables granular, user-level throttling and prevents users from bypassing limits by switching IP addresses.

--- middlewares/securityMiddleware.js
+++ middlewares/securityMiddleware.js
@@ -2,11 +2,14 @@ const { default: rateLimit } = require("express-rate-limit");
 const { default: helmet } = require("helmet");
 
 //! Rate Limiting middleware
-exports.limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!"
-});
+exports.limiter = (maxLimitig) =>
+  rateLimit({
+    //! Generate a unique key for each request based on the user's ID or email
+    keyGenerator: (req) => req.user?.id || req.body.email,
+    max: maxLimitig,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests, please try again in an hour!"
+  });
 
 //! Prevent XSS attacks and clickjacking vulnerabilities by setting the X-XSS-Protection header
 exports.customCSPProtection = () =>
